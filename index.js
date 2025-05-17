ed8471e0f0debf4d0a34e6f02b8e99157b3b0573
Angelito91 <portuondoangel@gmail.com>
2025-05-17 08:55:05 -0400

    Creando el sistema de autenticacion con json web token, un middleware para proteger las rutas, y un archivo config

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
+
+const config = require('./config');
+
+// Routes
 const expensesRoutes = require('./routes/expenses');
+const usersRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth')
+
 const { initTables } = require('./db/database');
 
 app.use(express.json());
 
+app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expensesRoutes);
+app.use('/api/users', usersRoutes);
 
 initTables()
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(config.db.port, () => {
+  console.log(`Servidor corriendo en http://localhost:${config.db.port}`);
 });
