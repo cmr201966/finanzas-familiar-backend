88542bc8c4de6f68deb6bd7e6dad1070c5bf5926
Carlos Mora <cmr201966@gmail.com>
2025-05-17 14:09:07 -0400

    routers users.js cometarios swagger

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,27 @@ import authMiddleware from '../middleware/auth.js';
 
 export const UsersRouter = Router()
 
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: Gestión de usuarios
+ */
+
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Obtener todos los usuarios
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios
+ *       500:
+ *         description: Error del servidor
+ */
+
 UsersRouter.get('/', authMiddleware, (req, res) => {
     UsersModel.getUsers((err, users) => {
         if (err) return res.status(500).send(err)
