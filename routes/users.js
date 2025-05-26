9064fc3f939fb8e6f2f37f3de3c51bc961e32ab0
Angelito91 <portuondoangel@gmail.com>
2025-05-25 20:01:43 -0400

    protegiendo las rutas

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,13 @@ import { Router } from 'express';
 // Models
 import { UsersModel } from '../models/users.js';
 
-// Middleware
-import authMiddleware from '../middleware/auth.js';
-
 // Helpers
 import { resSuccess, resError } from '../helpers/response.js';
 
 // Router
 export const UsersRouter = Router()
 
-UsersRouter.get('/', authMiddleware, (req, res) => {
+UsersRouter.get('/', (req, res) => {
     UsersModel.getUsers((err, users) => {
         if (err) return resError(res, { status: 500, message: 'Error del servidor' });
 
@@ -20,7 +17,7 @@ UsersRouter.get('/', authMiddleware, (req, res) => {
     })
 })
 
-UsersRouter.get('/:id', authMiddleware, (req, res) => {
+UsersRouter.get('/:id', (req, res) => {
     const { id } = req.params
     UsersModel.getUserById(id, (err, user) => {
         if (err) return resError(res, { status: 500, message: 'Error del servidor' });
